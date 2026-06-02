3968bdb7ae033a6b5a36830b58d09e4fc3489055
Bjoern Hansen <b.hansen.ubiqum@gmail.com>
2026-06-02 11:22:00 +0200

    remove all code related to the chat api solution

--- src/modules/airport/airport.module.ts
+++ src/modules/airport/airport.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GraphqlClientModule } from '../graphql-client/graphql-client.module';
-import { AirportChatController } from './airport-chat.controller';
 import { AirportService } from './airport.service';
 
 @Module({
   imports: [GraphqlClientModule],
-  controllers: [AirportChatController],
   providers: [AirportService],
   exports: [AirportService],
 })
